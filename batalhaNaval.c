02f46ba4a78dba1bebc352bf9ff629b76bf49426
Jefferson Lima <jeffersonlima32@gmail.com>
2025-06-09 11:43:37 +0000

    Jogo Batalha naval nível novato

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -3,12 +3,53 @@
 // Desafio Batalha Naval - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
+// Nível: novato
+
+int tabuleiro[10][10];
+int navio1[3]={3,3,3};
+int navio2[3]={3,3,3};
+
+void exibirTabuleiro(){
+    for(int i=0;i<10;i++){
+        for(int j=0;j<10;j++){
+            printf("%d ",tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+}
 
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    //Inicializando tabuleiro com zeros representando a água
+    for(int i=0;i<10;i++){
+        for(int j=0;j<10;j++){
+            tabuleiro[i][j]=0;
+        }
+    }
+    printf("Posicao inicial do tabuleirio:\n");
+    exibirTabuleiro();
+
+    //posicao inicial dos navios
+    int posicaoA[2]={2,3};//Horizontal
+    int posicaoB[2]={5,5};//Vertical
+    
+    
+    //alocando os navios no tabuleiro
+    //Navio 1
+    printf("\nAlocando Navio 1\n");
+    printf("Linha %d - Coluna %d\n",posicaoA[0],posicaoA[1]);
+    for(int i=posicaoA[0]-1,j=posicaoA[1]-1;j<=(posicaoA[0]+2);j++){
+        tabuleiro[i][j]=navio1[0];//preencher tabuleiro com o navio 1 na horizontal
+    }
+    //Navio 2
+    printf("\nAlocando Navio 2\n");
+    printf("Linha %d - Coluna %d\n",posicaoB[0],posicaoB[1]);
+    for(int i=posicaoB[0]-1,j=posicaoB[1]-1;i<=(posicaoB[0]+2);i++){
+        tabuleiro[i][j]=navio2[0];//preencher tabuleiro com o navio 1 na vertical
+    }
+    printf("\nTabuleiro atualizado:\n");
+    exibirTabuleiro();
+    return 0;
+}
 
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
@@ -35,6 +76,3 @@ int main() {
     // 0 0 1 0 0
     // 1 1 1 1 1
     // 0 0 1 0 0
-
-    return 0;
-}
