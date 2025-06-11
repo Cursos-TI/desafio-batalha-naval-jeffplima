fb8cf89b47e26821a85cdbedcd5ea76cce1d0024
Jefferson Lima <jeffersonlima32@gmail.com>
2025-06-11 00:39:15 +0000

    Atualizado para nivel Aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -3,12 +3,24 @@
 // Desafio Batalha Naval - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
-// Nível: novato
+// Nível: Aventureiro
 
+//Tabuleiro
 int tabuleiro[10][10];
+
+//Navios
 int navio1[3]={3,3,3};
 int navio2[3]={3,3,3};
+int navio3[3]={3,3,3};
+int navio4[3]={3,3,3};
 
+//Posicao inicial dos navios
+int posicaoA[2]={2,3};//Horizontal
+int posicaoB[2]={5,5};//Vertical
+int posicaoC[2]={8,1};//Diagonal
+int posicaoD[2]={3,6};//Diagonal
+
+//Função para exibir tabuleiro
 void exibirTabuleiro(){
     for(int i=0;i<10;i++){
         for(int j=0;j<10;j++){
@@ -20,41 +32,48 @@ void exibirTabuleiro(){
 
 int main() {
     //Inicializando tabuleiro com zeros representando a água
+    printf("\nInicializando o jogo!\nCriando tabuleiro!\n");
     for(int i=0;i<10;i++){
         for(int j=0;j<10;j++){
             tabuleiro[i][j]=0;
         }
     }
-    printf("Posicao inicial do tabuleirio:\n");
+    printf("Tabuleiro criado!\n");
     exibirTabuleiro();
-
-    //posicao inicial dos navios
-    int posicaoA[2]={2,3};//Horizontal
-    int posicaoB[2]={5,5};//Vertical
-    
-    
+ 
     //alocando os navios no tabuleiro
     //Navio 1
-    printf("\nAlocando Navio 1\n");
+    printf("\nAlocando Navio 1 - ");
     printf("Linha %d - Coluna %d\n",posicaoA[0],posicaoA[1]);
-    for(int i=posicaoA[0]-1,j=posicaoA[1]-1;j<=(posicaoA[0]+2);j++){
+    for(int i=posicaoA[0]-1,j=posicaoA[1]-1;j<=posicaoA[0]+2;j++){
         tabuleiro[i][j]=navio1[0];//preencher tabuleiro com o navio 1 na horizontal
     }
     //Navio 2
-    printf("\nAlocando Navio 2\n");
+    printf("\nAlocando Navio 2 - ");
     printf("Linha %d - Coluna %d\n",posicaoB[0],posicaoB[1]);
-    for(int i=posicaoB[0]-1,j=posicaoB[1]-1;i<=(posicaoB[0]+2);i++){
-        tabuleiro[i][j]=navio2[0];//preencher tabuleiro com o navio 1 na vertical
+    for(int i=posicaoB[0]-1,j=posicaoB[1]-1;i<(posicaoB[0]+2);i++){
+        tabuleiro[i][j]=navio2[0];//preencher tabuleiro com o navio 2 na vertical
     }
+
+    //Navio 3
+    printf("\nAlocando Navio 3 - ");
+    printf("Linha %d - Coluna %d\n",posicaoC[0],posicaoC[1]);
+    for(int i=posicaoC[0]-1,j=posicaoC[1]-1;i<=posicaoC[0]+2;i++,j++){
+        tabuleiro[i][j]=navio3[0];//preencher tabuleiro com o navio 3 na diagonal
+    }
+    
+    //Navio 4
+    printf("\nAlocando Navio 4 - ");
+    printf("Linha %d - Coluna %d\n",posicaoD[0],posicaoD[1]);
+    for(int i=posicaoD[0]-1,j=posicaoD[1]-1;i<posicaoD[0]+2;i++,j++){//tabuleiro -1
+        tabuleiro[i][j]=navio4[0];//preencher tabuleiro com o navio 4 na diagonal
+    }
+
     printf("\nTabuleiro atualizado:\n");
     exibirTabuleiro();
     return 0;
 }
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
 
     // Nível Mestre - Habilidades Especiais com Matrizes
     // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
